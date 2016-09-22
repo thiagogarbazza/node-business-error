41e51758b16353e09a3a60aa7efd91a5297abfe3
thiagogarbazza <thiagogarbazza@gmail.com>
2016-09-22 16:53:04 -0300

    fix promise create

--- src/abstract-validate.js
+++ src/abstract-validate.js
@@ -5,18 +5,15 @@ const compact = require('lodash').compact;
 class AbstractValidate {
 
   resolveValidationPromises(promises) {
-    return new Promise((resolve, reject) => {
-      Promise
-        .all(promises)
-        .then(cases => {
-          const businessCases = compact(cases);
-          if (businessCases.length > 0) {
-            return reject(new BusinessError('There was a business mistake.', businessCases));
-          }
-          return resolve();
-        })
-        .catch(error => reject(new BusinessError('There was an unexpected error business', error)));
-    });
+    return Promise.all(promises)
+      .then(cases => {
+        const businessCases = compact(cases);
+        if (businessCases.length > 0) {
+          return Promise.reject(new BusinessError('There was a business mistake.', businessCases));
+        }
+        return Promise.resolve();
+      })
+      .catch(error => Promise.reject(new BusinessError('There was an unexpected error business', error)));
   }
 }
 
